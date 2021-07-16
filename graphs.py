0b0feeb6ed568dc2430b75e8ac83fdd11c512291
Shira Goldhaber-Gordon <shirag624@gmail.com>
2021-07-16 09:43:36 -0600

    deleted grid graph

--- graphs.py
+++ graphs.py
@@ -79,35 +79,3 @@ graph("training-3-data.npy", "training-3-labels.npy", 3)
 
 
 exit()
-
-
-
-# #grid of 15 dnn predictions
-
-# def plot_value_array(i, predictions_array, true_label_array):
-#   true_label = true_label_array[i]
-#   plt.grid(False)
-#   plt.xticks(range(10))
-#   plt.yticks([])
-#   thisplot = plt.bar(range(2), predictions_array, color="#777777")
-#   plt.ylim([0, 1])
-#   predicted_label = np.argmax(predictions_array)
-
-#   thisplot[predicted_label].set_color('red')
-#   thisplot[true_label].set_color('blue')
-
-
-# #plot the first 15 waves/no waves, their predicted labels, and the true labels
-# #color correct predictions in blue and incorrect predictions in red
-# time = np.linspace(0, 100, num = 500000)
-# num_rows = 5
-# num_cols = 3
-# num_graphs = num_rows*num_cols
-# plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-# for i in range(num_graphs):
-#   plt.subplot(num_rows, 2*num_cols, 2*i+1)
-#   plt.plot(time, testing_data[i])
-#   plt.subplot(num_rows, 2*num_cols, 2*i+2)
-#   plot_value_array(i, predictions[i], testing_labels)
-# plt.tight_layout()
-# plt.show()
